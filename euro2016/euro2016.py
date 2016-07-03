6e5163ff6aa0fc9d3fff238e46bd538835f893eb
Christopher D. Long <galizur@gmail.com>
2016-07-03 13:33:25 -0400

    Euro 2016 bot.

---
+++ euro2016/euro2016.py
@@ -0,0 +1,531 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from twython import Twython
+from scipy.stats import skellam
+from urllib import FancyURLopener
+
+import psycopg2
+import json
+import time
+import os
+import sys
+
+# True - prints to screen and tweets
+# False - only prints to screen
+
+Tweet = True
+
+# FIFA's hidden API
+
+livematches_url = "http://www.fifa.com/Live/common/world-match-centre/livematches.js"
+
+# Need to pull from database
+
+ot_ft = 0.32715089107274
+
+# Stoppage stop assumptions
+# Generally want these to be conservative
+
+stoppage_1reg = 2
+stoppage_2reg = 4
+
+stoppage_1ot = 1
+stoppage_2ot = 1
+
+hashtag = "\n\n#EURO2016"
+
+competition = u"Euro 2016".encode('utf-8')
+
+def overtime(mu1, mu2, min, up, outcome):
+
+# Sloppy
+
+    if (min < 90):
+        aup = 0
+        amin = 90
+    else:
+        aup = up
+        amin = min
+    
+    if (amin <= 105): # 1st extra time
+        time_r = (120.0-amin)+stoppage_1ot+stoppage_2ot
+    elif (amin <= 120): # 2nd extra time
+        time_r = (120.0-amin)+stoppage_2ot
+
+    ft = time_r/(30.0+stoppage_1ot+stoppage_2ot)
+
+    if (outcome=="draw"):
+        p = skellam.pmf(-aup, ft*mu1*ot_ft, ft*mu2*ot_ft)
+    elif (outcome == "lose"):
+        p = skellam.cdf(-1-aup, ft*mu1*ot_ft, ft*mu2*ot_ft)
+    else:
+        p = skellam.cdf(-1+aup, ft*mu2*ot_ft, ft*mu1*ot_ft)
+
+    return(p)
+
+def outcome(mu1, mu2, min, up, outcome):
+
+# Sloppy
+
+    if (min > 90):
+        if (outcome=="draw"):
+            p = 1.0
+        else:
+            p = 0.0
+        return(p)
+
+    if (min <= 45): # 1st half
+        time_r = (90.0-min)+stoppage_1reg+stoppage_2reg
+    elif (min <= 90): # 2nd half
+        time_r = (90.0-min)+stoppage_2reg
+        
+    ft = time_r/(90.0+stoppage_1reg+stoppage_2reg)
+
+    if (outcome=="draw"):
+        p = skellam.pmf(-up, mu1*ft, mu2*ft)
+    elif (outcome == "lose"):
+        p = skellam.cdf(-1-up, mu1*ft, mu2*ft)
+    else:
+        p = skellam.cdf(-1+up, mu2*ft, mu1*ft)
+
+    return(p)
+
+# Using pgpass
+
+try:
+    conn = psycopg2.connect("host=localhost dbname=soccer user=clong")    
+except:
+    print("Database connection failed.")
+
+cur = conn.cursor()
+
+# Twitter app keys
+
+keys = json.loads(open(os.path.expanduser('~/.twitter_keys'), 'r').read())
+
+app_key = keys["app_key"]
+app_secret = keys["app_secret"]
+oauth_token = keys["oauth_token"]
+oauth_token_secret = keys["oauth_token_secret"]
+
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+
+# No robots, no problem
+
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0'
+
+myopener = MyOpener()
+    
+# Stoppage time - 1.25 minutes 1st half, 3 minutes 2nd half
+# To be conservative - 2 minutes 1st half, 4 minutes 2nd half
+
+# Again, this part needs to be automated
+
+ids = [int(x) for x in sys.argv[1:]]
+
+#300360759
+
+names = {}
+names[300360759] = ["France", "Iceland"]
+
+team_name = {}
+team_name["fra"] = "France"
+team_name["isl"] = "Iceland"
+
+team_ids = {}
+team_ids[300360759] = ["fra", "isl"]
+
+# Possibly for future use
+
+#fields = {}
+#fields[300269493] = "neutral"
+#fields[300269496] = "neutral"
+
+score = {}
+score[300360759] = [0, 0]
+
+status = {}
+status[300360759] = ""
+
+home_p = {}
+home_p[300360759] = None
+
+# Possible status
+# Match starting time (local)
+# "fifa.lineups"
+# "fifa.end1sthalf"
+# "fifa.end2ndhalf"
+# "result"
+
+select_mu = "select * from fifa.men_mu where team_id='%s' and opponent_id='%s'"
+
+mu = {}
+
+print
+
+for id in ids:
+    home_id = team_ids[id][0]
+    away_id = team_ids[id][1]
+    home_name = team_name[home_id]
+    away_name = team_name[away_id]
+    string = "%s\n%s - %s\n\n" % (competition, home_name, away_name)
+    #print "%s game %s - %s" % (competition, home_name, away_name)
+
+    select = select_mu % (home_id, away_id)
+
+    cur.execute(select)
+    row = cur.fetchone()
+
+    mu1 = row[4]
+    mu2 = row[5]
+
+    mu[id] = [mu1, mu2]
+
+    home_reg = outcome(mu1, mu2, 0, 0, "win")
+    away_reg = outcome(mu1, mu2, 0, 0, "lose")
+    draw_reg = outcome(mu1, mu2, 0, 0, "draw")
+
+    # With extra time
+
+    home_ot = overtime(mu1, mu2, 90, 0, "win")
+    away_ot = overtime(mu1, mu2, 90, 0, "lose")
+    draw_ot = overtime(mu1, mu2, 90, 0, "draw")
+    home_win = home_reg + draw_reg*(home_ot + 0.5*draw_ot)
+    away_win = 1.0 - home_win
+
+    # Without extra time
+    
+    #home_win = home_reg + draw_reg*0.5
+    #away_win = 1.0 - home_win
+    
+    string += "%s wins - %.1f%%\n" % (home_name, home_win*100)
+    string += "%s wins - %.1f%%" % (away_name, away_win*100)
+    string += hashtag
+    print string
+
+   if Tweet:
+       print "Tweeting ..."
+       twitter.update_status(status=string)
+
+while True:
+
+    time.sleep(15)
+
+    #response = ""
+    response = None
+    while response is None:
+        try:
+            response = myopener.open(livematches_url).read()
+        except:
+            print "Sleeping ..."
+            time.sleep(15)
+            pass
+
+    # jsonp
+    
+    #live_json = jsonp[ jsonp.index("(")+1 : jsonp.rindex(")") ]
+    #live = json.loads(live_json)
+
+    # json
+    
+    live = json.loads(response)
+
+    # To test with a local file:
+
+    #live = json.loads(open('samples/livematches2.js', 'r').read())
+
+    for game in live["matches"]:
+
+        game_id = int(game["id"])
+
+        if not(game_id in ids):
+            continue
+
+        old_status = status[game_id]
+        new_status = game["min"]
+        
+        if (new_status==old_status):
+            continue
+    
+        r = game["r"]
+        s = game["s"]
+
+        home = names[game_id][0]
+        away = names[game_id][1]
+        mu1 = mu[game_id][0]
+        mu2 = mu[game_id][1]
+
+        if "-" in r:
+            home_score = int(r.split("-")[0])
+            away_score = int(r.split("-")[1])
+            if (home_score > score[game_id][0]):
+                string = "%s goal!%s" % (home, hashtag)
+                print string
+                print
+                score[game_id][0] = home_score
+                
+                #if Tweet:
+                #    print "Tweeting ..."
+                #    twitter.update_status(status=string)
+                #    time.sleep(5)
+                    
+            if (away_score > score[game_id][1]):
+                string = "%s goal!%s" % (away, hashtag)
+                print string
+                print
+                score[game_id][1] = away_score
+                
+                #if Tweet:
+                #    print "Tweeting ..."
+                #    twitter.update_status(status=string)
+                #    time.sleep(5)
+                    
+        else:
+            continue
+
+        if (new_status == "fifa.half-time"):
+            status[game_id] = new_status
+        
+            min = 45
+
+            home_reg = outcome(mu1, mu2, min, home_score-away_score, "win")
+            away_reg = outcome(mu1, mu2, min, home_score-away_score, "lose")
+            draw_reg = 1.0 - home_reg - away_reg
+
+            home_ot = overtime(mu1, mu2, min, home_score-away_score, "win")
+            away_ot = overtime(mu1, mu2, min, home_score-away_score, "lose")
+            draw_ot = 1.0 - home_ot - away_ot
+
+            home_win = home_reg + draw_reg*(home_ot + 0.5*draw_ot)
+            away_win = 1.0 - home_win
+            
+            #home_win = home_reg + draw_reg*0.5
+            #away_win = 1.0 - home_win
+
+            hw_string = "%.1f%%" % (home_win*100)
+            aw_string = "%.1f%%" % (away_win*100)
+            status[game_id] = new_status
+        
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            string = string + "Half-time\n"
+            string = string + home + " wins " + hw_string + "\n"
+            string = string + away + " wins " + aw_string
+            string += hashtag
+            print string
+            print
+
+            if Tweet:
+                print "Tweeting ..."
+                twitter.update_status(status=string)
+                time.sleep(5)
+
+        elif new_status in ("fifa.endfirstextra"):
+
+            status[game_id] = new_status
+        
+            min = 105
+            home_reg = outcome(mu1, mu2, min, home_score-away_score, "win")
+            away_reg = outcome(mu1, mu2, min, home_score-away_score, "lose")
+            draw_reg = 1.0 - home_reg - away_reg
+
+            home_ot = overtime(mu1, mu2, min, home_score-away_score, "win")
+            away_ot = overtime(mu1, mu2, min, home_score-away_score, "lose")
+            draw_ot = 1.0 - home_ot - away_ot
+
+            home_win = home_reg + draw_reg*(home_ot + 0.5*draw_ot)
+            away_win = 1.0 - home_win
+
+            #home_win = home_reg + draw_reg*0.5
+            #away_win = 1.0 - home_win
+
+            hw_string = "%.1f%%" % (home_win*100)
+            aw_string = "%.1f%%" % (away_win*100)
+        
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            string = string + "End of 1st extra\n"
+            string = string + home + " wins " + hw_string + "\n"
+            string = string + away + " wins " + aw_string
+            string += hashtag
+            print string
+            print
+
+            if Tweet:
+                print "Tweeting ..."
+                twitter.update_status(status=string)
+                time.sleep(5)
+
+        elif new_status in ("fifa.endsecondextra"):
+
+            status[game_id] = new_status
+        
+            home_win = 0.5
+            away_win = 0.5
+
+            hw_string = "%.1f%%" % (home_win*100)
+            aw_string = "%.1f%%" % (away_win*100)
+        
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            string = string + "Entering penalty kicks\n"
+            string = string + home + " wins " + hw_string + "\n"
+            string = string + away + " wins " + aw_string
+            string += hashtag
+            print string
+            print
+
+            if Tweet:
+                print "Tweeting ..."
+                twitter.update_status(status=string)
+                time.sleep(5)
+
+        elif new_status in ("fifa.penaltiesphase"):
+
+            status[game_id] = new_status
+        
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            string = string + "Entering penalty kicks\n"
+            string = string + home + " wins " + hw_string + "\n"
+            string = string + away + " wins " + aw_string
+            string += hashtag
+            print string
+            print
+
+            if Tweet:
+                print "Tweeting ..."
+                twitter.update_status(status=string)
+                time.sleep(5)
+            
+        elif new_status in ("fifa.end2ndhalf"):
+            
+            status[game_id] = new_status
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            
+            if (home_score==away_score):
+                #string = string + "Entering penalty kicks\n"
+                string = string + "Entering extra time\n"
+                string += hashtag
+                print string
+                print
+
+                if Tweet:
+                    print "Tweeting ..."
+                    twitter.update_status(status=string)
+                    time.sleep(5)
+
+        elif new_status in ("fifa.full-time"):
+            
+            status[game_id] = new_status
+            string =  "%s-%s : %s-%s" % (home,away,home_score,away_score)
+
+            # This bombed
+            scorepenh = game["scorepenh"]
+            scorepena = game["scorepena"]
+
+            if (scorepenh<>""):
+                string += " ("+scorepenh+"-"+scorepena+")\n"
+            else:
+                string += "\n"
+            
+            string = string + "Game is over\n"
+            if (home_score>away_score):
+                string = string + home + " wins"
+            elif (home_score<away_score):
+                string = string + away + " wins"
+            else:
+                scorepenh = game["scorepenh"]
+                scorepena = game["scorepena"]
+                if (scorepenh>scorepena):
+                    string = string + home + " wins"
+                else:
+                    string = string + away + " wins"
+
+            string += hashtag
+            print string
+            print
+
+            if Tweet:
+                print "Tweeting ..."
+                twitter.update_status(status=string)
+                time.sleep(5)
+
+        elif "'" in new_status:
+
+            status[game_id] = new_status
+
+            stoppage = False
+            if "+" in new_status:
+                stoppage = True
+                min = new_status.replace("+","").strip()
+                min = int(min.replace("'",""))
+            else:
+                stoppage = False
+                min = int(new_status.replace("'",""))
+
+            home_reg = outcome(mu1, mu2, min, home_score-away_score, "win")
+            away_reg = outcome(mu1, mu2, min, home_score-away_score, "lose")
+            draw_reg = 1.0 - home_reg - away_reg
+            
+            home_ot = overtime(mu1, mu2, min, home_score-away_score, "win")
+            away_ot = overtime(mu1, mu2, min, home_score-away_score, "lose")
+            draw_ot = 1.0 - home_ot - away_ot
+            
+            home_win = home_reg + draw_reg*(home_ot + 0.5*draw_ot)
+            away_win = 1.0 - home_win
+
+            #home_win = home_reg + draw_reg*0.5
+            #away_win = 1.0 - home_win
+
+            hw_string = "%.1f%%" % (home_win*100)
+            aw_string = "%.1f%%" % (away_win*100)
+
+        
+            string =  "%s-%s : %s-%s\n" % (home,away,home_score,away_score)
+            string += "time = " + new_status
+            
+            string += "\n"
+
+            if (home_p[id] == None) or (abs(home_win-home_p[id])>=0.01):
+
+                if (home_p[id] == None):
+                    home_delta = 0.00
+                    away_delta = 0.00
+                else:
+                    home_delta = home_win-home_p[id]
+                    away_delta = -home_delta
+
+                if (home_delta>0.0):
+                    home_change = " | +%.1f%%" % (home_delta*100)
+                    away_change = ""
+                    #away_change = " (%.1f%%)" % (away_delta*100)
+                elif (home_delta<0.0):
+                    #home_change = " (%.1f%%)" % (home_delta*100)
+                    home_change = ""
+                    away_change = " | +%.1f%%" % (away_delta*100)
+
+                if (home_p[id] == None):
+                    string += home + " wins " + hw_string + "\n"
+                    string += away + " wins " + aw_string
+                else:
+                    string += home + " wins " + hw_string + home_change + "\n"
+                    string += away + " wins " + aw_string + away_change
+
+                home_p[id] = home_win
+
+                string += hashtag
+                print string
+                print
+
+                if Tweet:
+                    print "Tweeting ..."
+                    twitter.update_status(status=string)
+                    time.sleep(5)
+            else:
+                print string
+                print
+                continue
+                
+        else:
+            status[game_id] = new_status
+            print new_status
+            print
---
+++ euro2016/mu.sql
@@ -0,0 +1,119 @@
+begin;
+
+drop table if exists fifa.men_mu;
+
+create table fifa.men_mu (
+       team_id	       text,
+       team_name       text,
+       opponent_id     text,
+       opponent_name   text,
+       team_mu	       float,
+       opponent_mu     float,
+       primary key (team_id,opponent_id)
+);
+
+insert into fifa.men_mu
+(team_id,team_name,opponent_id,opponent_name,team_mu,opponent_mu)
+(
+select
+s1.team_id,t1.team_name,
+s2.team_id,t2.team_name,
+
+exp(bf.estimate)*h.offensive*v.defensive,
+exp(bf.estimate)*v.offensive*h.defensive
+
+from fifa.men_schedule_factors s1
+join fifa.men_schedule_factors s2
+  on (s1.team_id<>s2.team_id)
+join fifa.teams t1
+  on (t1.team_id,t1.gender_id)=(s1.team_id,'men')
+join fifa.teams t2
+  on (t2.team_id,t2.gender_id)=(s2.team_id,'men')
+join fifa.men_schedule_factors h
+  on (h.team_id)=(t1.team_id)
+join fifa.men_schedule_factors v
+  on (v.team_id)=(t2.team_id)
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+
+where
+
+    s1.team_id in
+    ('ger','isl')
+and s2.team_id in
+    ('ger','isl')
+);
+
+insert into fifa.men_mu
+(team_id,team_name,opponent_id,opponent_name,team_mu,opponent_mu)
+(
+select
+s1.team_id,t1.team_name,
+s2.team_id,t2.team_name,
+
+exp(bf.estimate)*h.offensive*v.defensive*o.exp_factor,
+exp(bf.estimate)*v.offensive*h.defensive*d.exp_factor
+
+from fifa.men_schedule_factors s1
+join fifa.men_schedule_factors s2
+  on (s1.team_id<>s2.team_id)
+join fifa.teams t1
+  on (t1.team_id,t1.gender_id)=(s1.team_id,'men')
+join fifa.teams t2
+  on (t2.team_id,t2.gender_id)=(s2.team_id,'men')
+join fifa.men_schedule_factors h
+  on (h.team_id)=(t1.team_id)
+join fifa.men_schedule_factors v
+  on (v.team_id)=(t2.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+
+where
+
+    s1.team_id in
+    ('fra')
+and s2.team_id in
+    ('ger','isl')
+);
+
+insert into fifa.men_mu
+(team_id,team_name,opponent_id,opponent_name,team_mu,opponent_mu)
+(
+select
+s1.team_id,t1.team_name,
+s2.team_id,t2.team_name,
+
+exp(bf.estimate)*h.offensive*v.defensive*d.exp_factor,
+exp(bf.estimate)*v.offensive*h.defensive*o.exp_factor
+
+from fifa.men_schedule_factors s1
+join fifa.men_schedule_factors s2
+  on (s1.team_id<>s2.team_id)
+join fifa.teams t1
+  on (t1.team_id,t1.gender_id)=(s1.team_id,'men')
+join fifa.teams t2
+  on (t2.team_id,t2.gender_id)=(s2.team_id,'men')
+join fifa.men_schedule_factors h
+  on (h.team_id)=(t1.team_id)
+join fifa.men_schedule_factors v
+  on (v.team_id)=(t2.team_id)
+join fifa.men_factors o
+  on (o.parameter,o.level)=('field','offense_home')
+join fifa.men_factors d
+  on (d.parameter,d.level)=('field','defense_home')
+join fifa.men_basic_factors bf
+  on bf.factor='(Intercept)'
+
+where
+
+    s1.team_id in
+    ('ger','isl')
+and s2.team_id in
+    ('fra')
+);
+
+commit;
